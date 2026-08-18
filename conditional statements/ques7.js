406eed49c0772e96a429dc7054bb46f79fdc3a19
Rajat <rajatjangra2260@gmail.com>
2026-08-18 18:18:55 +0530

    Add files via upload

---
+++ conditional statements/ques7.js
@@ -0,0 +1,7 @@
+var age = 20
+if(age <=18 ){
+    console.log ("not eligible")
+}
+else {
+    console.log ("eligible")
+}
---
+++ conditional statements/ques9.js
@@ -0,0 +1,10 @@
+var quantity = 6
+if (quantity >10){
+    console.log("20% discount")
+}
+else if (quantity >5){
+    console.log("10% discount")
+}
+else {
+    console.log("no discount")
+}
